63f2863721335a8abf8633feadbfa299ccc1c9d7
18219482179 <tanghan4811@gmail.com>
2026-08-12 17:05:10 +0800

    initial commit

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -324,12 +324,6 @@ async function getInitConfig(
     CustomCategories: [],
     LiveConfig: [],
     PanSouConfig: getDefaultPanSouConfig(),
-    NetDiskConfig: {
-      enabled: true,
-      pansouUrl: 'https://so.252035.xyz',
-      timeout: 30,
-      enabledCloudTypes: ['baidu', 'aliyun', 'quark'],
-    },
     TMDBConfig: {
       ApiKey: process.env.TMDB_API_KEY || '',
       ProxyType: process.env.TMDB_REVERSE_PROXY
